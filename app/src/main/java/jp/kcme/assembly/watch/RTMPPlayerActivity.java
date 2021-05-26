de3d37af3b843e12b2ec9a5e86e1fe0e02139702
gia <nghiale1994@gmail.com>
2021-05-26 16:50:32 +0900

    More logs

--- app/src/main/java/jp/kcme/assembly/watch/RTMPPlayerActivity.java
+++ app/src/main/java/jp/kcme/assembly/watch/RTMPPlayerActivity.java
@@ -2,6 +2,7 @@ package jp.kcme.assembly.watch;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -17,30 +18,59 @@ import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 
 public class RTMPPlayerActivity extends CommonActivity {
+    SimpleExoPlayer player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        Log.i(AppUtils.get().tag(), "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rtmp_player);
+    }
 
-        // Create Simple ExoPlayer
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(AppUtils.get().tag(), "onResume");
 
-        PlayerView playerView = findViewById(R.id.simple_player);
+        try {
+            // Create Simple ExoPlayer
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+            TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+            player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
 
-        playerView.setPlayer(player);
+            PlayerView playerView = findViewById(R.id.simple_player);
+            playerView.setPlayer(player);
 
-        // Create RTMP Data Source
-        RtmpDataSourceFactory rtmpDataSourceFactory = new RtmpDataSourceFactory();
+            // Create RTMP Data Source
+            RtmpDataSourceFactory rtmpDataSourceFactory = new RtmpDataSourceFactory();
+            MediaSource videoSource = new ExtractorMediaSource
+                    .Factory(rtmpDataSourceFactory)
+                    .createMediaSource(Uri.parse("rtmp://beetle.mstgikai.com/live/" + getIntent().getStringExtra("channelId")));
 
-        MediaSource videoSource = new ExtractorMediaSource
-                .Factory(rtmpDataSourceFactory)
-                .createMediaSource(Uri.parse("rtmp://beetle.mstgikai.com/live/" + getIntent().getStringExtra("channelId")));
+            player.prepare(videoSource);
+            player.setPlayWhenReady(true);
 
-        player.prepare(videoSource);
-        player.setPlayWhenReady(true);
+        } catch (Exception e) {
+            Log.i(AppUtils.get().tag(), "Player Exception e: " + e);
+            Log.i(AppUtils.get().tag(), "Player Exception e's message: " + e.getMessage());
+            Log.i(AppUtils.get().tag(), "Player Exception e's cause: " + e.getCause());
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(AppUtils.get().tag(), "onStop");
+        super.onStop();
+
+        try {
+            player.release();
+
+        } catch (Exception e) {
+            Log.i(AppUtils.get().tag(), "Player Exception e: " + e);
+            Log.i(AppUtils.get().tag(), "Player Exception e's message: " + e.getMessage());
+            Log.i(AppUtils.get().tag(), "Player Exception e's cause: " + e.getCause());
+        }
     }
 }
